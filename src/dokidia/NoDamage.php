8727ef6676381dde9ab3e616fc198071070ff286
richenhub <74600494+richenhub@users.noreply.github.com>
2021-01-25 11:04:16 +0300

    Update NoDamage.php

--- src/dokidia/NoDamage.php
+++ src/dokidia/NoDamage.php
@@ -2,26 +2,20 @@
 
 namespace dokidia;
 
-use pocketmine\Player;
-use pocketmine\event\entity\EntityDamageEvent;
-use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
-
-class NoDamage extends PluginBase implements Listener {
-
+class NoDamage extends \pocketmine\plugin\PluginBase implements \pocketmine\event\Listener
+{
       public function onEnable() {
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-    }
-      
-      public function NoDamage(EntityDamageEvent $event){
-        $player = $event->getPlayer();
-            
-       if($player->isOp())
-       {
-             $damage->setCancelled(true);
-             $player->sendMessage("Damage Cancelled");
-       } else {
-             $damage->setCancelled(false);
-             $player->sendMessage("Damage Hasn't Cancelled");
+            $this->getServer()->getPluginManager()->registerEvents($this, $this);
+      }
+
+      public function NoDamage(\pocketmine\event\entity\EntityDamageEvent $event){
+            $player = $event->getPlayer();
+            if ($player->isOp()) {
+                  $event->setCancelled(true);
+                  $player->sendMessage("Damage Cancelled");
+            } else {
+                  $event->setCancelled(false);
+                  $player->sendMessage("Damage Hasn't Cancelled");
+            }
       }
 }
